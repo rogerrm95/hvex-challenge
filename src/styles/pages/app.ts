87d1ddba8a7fa083f534b6edcd062969e5f37edb
rogerrm95 <rogerrm@live.com>
2023-03-27 16:39:21 -0300

    fix: add overflow - hidden on AppContainer to fix white column bug

--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const AppContainer = styled.main`
   background-color: ${(props) => props.theme.colors.white};
   width: 100%;
+  overflow: hidden;
 
   display: flex;
   flex-direction: column;
